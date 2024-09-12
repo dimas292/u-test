6f056a2dd0dc997628f2ad2315154431b60c9466
dimas <dimasixc22@gmail.com>
2024-09-12 21:56:12 +0700

    feat : solving two bug

--- test/request-param.test.js
+++ test/request-param.test.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import request from 'supertest';
+import winston from 'winston';
 
+const logger = winston.createLogger({
+  level: 'info',
+  transports : [
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  ]
+})
 
-test('Request', () => {
+test('Request',async () => {
   const app = express();
-  app.get('/', async (req, res) => {
+  app.get('/',  (req, res) => {
     res.send(`Hai, ${req.query.firsname} ${req.query.lastname}`);
 
-    const response = await request(app).get('/').query({firsname: "dimas", lastname: "dimas"});
-    expect(response.text).toBe('Hai, dimas dimas');
   });
+  const response = await request(app).get('/').query({firsname: "dimas", lastname: "dimas"});
+  expect(response.text).toBe('Hai, dimas dimas');
+  logger.info(`${new Date()} ${response.text}`)
 });
